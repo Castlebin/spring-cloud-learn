c6f0e9400cc3463a5751a574b9e1080d8b20dff7
heller <heller@gmail.com>
2018-07-14 02:49:55 +0800

    方便识别返回来自哪个服务提供者

--- eureka-client/src/main/java/com/heller/eureka/client/controller/HelloController.java
+++ eureka-client/src/main/java/com/heller/eureka/client/controller/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController {
     @RequestMapping("/sayHello")
     public String sayHello(String name, HttpServletRequest request) {
         String host = request.getServerName() + request.getServerPort();
-        return "Hello, " + name + ", from host:" + host;
+        return "Hello, " + name + ", from host: " + host;
     }
 
 }
