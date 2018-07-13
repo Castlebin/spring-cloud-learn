862692c17f090e0377cc9e3414cb576b68cea1d1
heller <heller@gmail.com>
2018-07-14 01:19:44 +0800

    1. @EnableDiscoveryClient 并非必须， @EnableDiscoveryClient 并非必须，只要classpath中有DiscoveryClient 的实现类即可自动完成注册（更符合Spring Boot的启动逻辑） 2. 显示指定了要注册到的Eureka Server地址（eureka.client.serviceUrl.defaultZone）

--- eureka-client/src/main/java/com/heller/eureka/client/Application.java
+++ eureka-client/src/main/java/com/heller/eureka/client/Application.java
@@ -2,9 +2,7 @@ package com.heller.eureka.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
