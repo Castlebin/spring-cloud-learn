7b73328ddaf344bf9838775bf7e65d223387cb40
heller <heller@gmail.com>
2018-07-14 02:28:25 +0800

    方便识别返回来自哪个服务提供者

--- eureka-client/src/main/java/com/heller/eureka/client/controller/HelloController.java
+++ eureka-client/src/main/java/com/heller/eureka/client/controller/HelloController.java
@@ -3,13 +3,16 @@ package com.heller.eureka.client.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RequestMapping("/hello")
 @RestController
 public class HelloController {
 
     @RequestMapping("/sayHello")
-    public String sayHello(String name) {
-        return "Hello, " + name;
+    public String sayHello(String name, HttpServletRequest request) {
+        String host = request.getServerName() + request.getServerPort();
+        return "Hello, " + name + ", from host:" + host;
     }
 
 }
