f437623bc192f1a14163461c538996c18bee97fb
heller <heller@gmail.com>
2018-07-14 02:31:49 +0800

    使用LoadBalancerClient和RestTemplate来实现的服务消费者

---
+++ eureka-consumer/src/main/java/com/heller/eureka/consumer/controller/ConsumerController.java
@@ -0,0 +1,28 @@
+package com.heller.eureka.consumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/consumer")
+public class ConsumerController {
+
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping
+    public String dc() {
+        ServiceInstance serviceInstance = loadBalancerClient.choose("eureka-client");
+        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/hello/sayHello";
+        System.out.println(url);
+        return restTemplate.getForObject(url, String.class);
+    }
+
+}
