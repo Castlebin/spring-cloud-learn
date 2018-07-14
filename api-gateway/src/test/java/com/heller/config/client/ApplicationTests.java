3c51f99c80ff7244570c4c8247ccf8e9668fa9cd
heller <heller@gmail.com>
2018-07-14 16:42:03 +0800

    @EnableZuulProxy 轻松搭建服务网关（spring-cloud-starter-netflix-zuul），网关也注册到Eureka Server上

---
+++ api-gateway/src/test/java/com/heller/config/client/ApplicationTests.java
@@ -0,0 +1,20 @@
+package com.heller.config.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
