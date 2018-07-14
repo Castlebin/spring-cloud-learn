c4eff9a827928ec2784521ccf398d4026053b763
heller <heller@gmail.com>
2018-07-14 17:11:57 +0800

    @EnableZuulProxy 轻松搭建服务网关（spring-cloud-starter-netflix-zuul），网关也注册到Eureka Server上

--- api-gateway/src/main/java/com/heller/config/gateway/Application.java
+++ api-gateway/src/main/java/com/heller/config/gateway/Application.java
@@ -1,4 +1,4 @@
-package com.heller.config.client;
+package com.heller.config.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- api-gateway/src/test/java/com/heller/config/gateway/ApplicationTests.java
+++ api-gateway/src/test/java/com/heller/config/gateway/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.heller.config.client;
+package com.heller.config.gateway;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
