62d080283e19c153015c97d2702fce796d7ef04d
heller <heller@gmail.com>
2018-07-15 16:56:11 +0800

    Spring Boot Admin (spring-boot-admin-starter-server、@EnableAdminServer)
    Spring Boot Admin
    项目地址：https://github.com/codecentric/spring-boot-admin

---
+++ admin-server/src/main/java/com/heller/admin/server/Application.java
@@ -0,0 +1,22 @@
+package com.heller.admin.server;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableAdminServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
