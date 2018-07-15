e3602d4efdc09682de94edb1761417324ca4e012
heller <heller@gmail.com>
2018-07-15 16:16:53 +0800

    使用Turbie聚合Hystrix端点（spring-cloud-starter-netflix-turbine，@EnableTurbine），Turbine聚合所有hystrix.stream端点到/turbine.stream下
    turbine:
      clusterNameExpression: new String("default")
      # 配置要聚合的Hystrix客户端
      appConfig: eureka-consumer, eureka-consumer-ribbon, eureka-consumer-feign
    
    turbine.appConfig指定要聚合的服务列表

---
+++ eureka-monitor-turbine/src/main/java/com/heller/eureka/consumer/Application.java
@@ -0,0 +1,15 @@
+package com.heller.eureka.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@EnableTurbine
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
