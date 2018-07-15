ac35c06015f8d59568b53d407dc0d6df755e0187
heller <heller@gmail.com>
2018-07-15 14:24:38 +0800

    Feign开启hystrix.stream端点监控（spring-cloud-starter-netflix-hystrix，management.endpoints.web.exposure.include: hystrix.stream，@EnableCircuitBreaker三个条件）

--- eureka-consumer-feign/src/main/java/com/heller/eureka/consumer/Application.java
+++ eureka-consumer-feign/src/main/java/com/heller/eureka/consumer/Application.java
@@ -2,8 +2,10 @@ package com.heller.eureka.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableCircuitBreaker
 @EnableFeignClients
 @SpringBootApplication
 public class Application {
