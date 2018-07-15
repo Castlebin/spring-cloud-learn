7a4a7fb4dd8223551f2ec87594dfe5d6e70725e0
heller <heller@gmail.com>
2018-07-15 14:29:25 +0800

    Feign集成Hystrix Dashboard（spring-cloud-starter-netflix-hystrix-dashboard，@EnableHystrixDashboard即可）
    （注意开启hystrix.stream支持，spring-cloud-starter-netflix-hystrix，management.endpoints.web.exposure.include: hystrix.stream，@EnableCircuitBreaker三个条件）

--- eureka-consumer-feign/src/main/java/com/heller/eureka/consumer/Application.java
+++ eureka-consumer-feign/src/main/java/com/heller/eureka/consumer/Application.java
@@ -3,8 +3,10 @@ package com.heller.eureka.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableHystrixDashboard
 @EnableCircuitBreaker
 @EnableFeignClients
 @SpringBootApplication
