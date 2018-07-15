fef76a0868ee7a6852b9341d2808d311f478e290
heller <heller@gmail.com>
2018-07-15 13:36:55 +0800

    @EnableHystrixDashboard(spring-cloud-starter-netflix-hystrix-dashboard)

--- eureka-consumer/src/main/java/com/heller/eureka/consumer/Application.java
+++ eureka-consumer/src/main/java/com/heller/eureka/consumer/Application.java
@@ -3,9 +3,11 @@ package com.heller.eureka.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableHystrixDashboard
 @EnableCircuitBreaker
 @SpringBootApplication
 public class Application {
