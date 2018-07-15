3823d9228ff2bdcb328dfbbdabd8a001902a8dd8
heller <heller@gmail.com>
2018-07-15 14:07:14 +0800

    @EnableHystrixDashboard(spring-cloud-starter-netflix-hystrix-dashboard) 注意需要将hystrix-dashboard端点监控加入到actuator端点中（management.endpoints.web.exposure.include: hystrix.stream），这样就开启了Hystrix客户端的/actuator/hystrix.stream监控端点。 在Hystrix-Dashboard中（${YOUT_HOST}/hystrix）中就可以输入Hystrix客户端的端点地址进行监控

--- eureka-consumer-ribbon/src/main/java/com/heller/eureka/consumer/Application.java
+++ eureka-consumer-ribbon/src/main/java/com/heller/eureka/consumer/Application.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableHystrixDashboard
 @EnableCircuitBreaker
 @SpringBootApplication
 public class Application {
