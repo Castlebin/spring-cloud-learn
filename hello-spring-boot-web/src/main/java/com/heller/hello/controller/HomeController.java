d1b530b9427aee51b9b3a27675c7aea012adddb2
heller <heller@gmail.com>
2018-07-21 15:13:24 +0800

    并没有使用@Autowired，这里使用的构造器方式的注入，Spring容器自动查找需要的bean进行注入了 这种方式不需要显式的去注解（但是使用起来不方便，推荐注解方式）

--- hello-spring-boot-web/src/main/java/com/heller/hello/controller/HomeController.java
+++ hello-spring-boot-web/src/main/java/com/heller/hello/controller/HomeController.java
@@ -13,6 +13,11 @@ public class HomeController {
 
     private HomeService homeService;
 
+    /**
+     * 并没有使用@Autowired，这里使用的构造器方式的注入，Spring容器自动查找需要的bean进行注入了
+     * 这种方式不需要显式的去注解（但是使用起来不方便，推荐注解方式）
+     * @param homeService
+     */
     public HomeController(HomeService homeService) {
         this.homeService = homeService;
     }
