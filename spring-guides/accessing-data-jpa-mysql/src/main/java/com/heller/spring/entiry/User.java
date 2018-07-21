90dfb892b36aa59a14925303371290b8e6fd77a6
heller <heller@gmail.com>
2018-07-21 17:59:11 +0800

    Accessing Data with JPA MySQL
    ( GenerationType.IDENTITY 终于使用的是mysql的表自增id了)

--- spring-guides/accessing-data-jpa-mysql/src/main/java/com/heller/spring/entiry/User.java
+++ spring-guides/accessing-data-jpa-mysql/src/main/java/com/heller/spring/entiry/User.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Date;
 
@@ -14,7 +15,7 @@ import java.util.Date;
 public class User {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
