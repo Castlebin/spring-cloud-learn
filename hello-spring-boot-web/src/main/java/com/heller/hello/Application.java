3cc689d7fa525d57684c91674a3274070ba8bf96
heller <heller@gmail.com>
2018-07-21 13:06:42 +0800

    增加测试

--- hello-spring-boot-web/src/main/java/com/heller/hello/Application.java
+++ hello-spring-boot-web/src/main/java/com/heller/hello/Application.java
@@ -13,6 +13,7 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        System.out.println("应用启动完毕...");
     }
 
     @Bean
