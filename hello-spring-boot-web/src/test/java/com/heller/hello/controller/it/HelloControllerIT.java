20792c39eae63ab9c1b26bf1b25e7c411b57485f
heller <heller@gmail.com>
2018-07-21 14:37:10 +0800

    直接测试web层，不启动一个完整的web服务（其实也差不多了 almost the full stack） In this test, the full Spring application context is started, but without the server.
    （使用Spring的MockMvc by @AutoConfigureMockMvc注解）

--- hello-spring-boot-web/src/test/java/com/heller/hello/controller/it/HelloControllerIT.java
+++ hello-spring-boot-web/src/test/java/com/heller/hello/controller/it/HelloControllerIT.java
@@ -19,6 +19,8 @@ import static org.junit.Assert.assertThat;
  * HelloController的集成测试，IT, integration test
  * 在一个随机端口启动web容器，启动了完整的web服务
  *
+ * In this test, the full Spring application context is started, but without the server.
+ *
  * Note the use of webEnvironment=RANDOM_PORT to start the server with a random port
  * (useful to avoid conflicts in test environments),
  * and the injection of the port with @LocalServerPort.
