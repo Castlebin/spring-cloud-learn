f0da2fdfaf953b7e482e10e470782464a8c450b7
heller <heller@gmail.com>
2018-07-21 14:24:07 +0800

    使用了AssertJ提供的断言

--- hello-spring-boot-web/src/test/java/com/heller/hello/SmokeTest.java
+++ hello-spring-boot-web/src/test/java/com/heller/hello/SmokeTest.java
@@ -7,6 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * 使用的是AssertJ提供的断言
+ */
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
