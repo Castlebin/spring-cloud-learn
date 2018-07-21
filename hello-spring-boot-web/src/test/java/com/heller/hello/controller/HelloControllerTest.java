0bffebfc9e0edfc22a2d33e91935986ee3e4a060
heller <heller@gmail.com>
2018-07-21 14:35:55 +0800

    直接测试web层，不启动一个完整的web服务（其实也差不多了 almost the full stack）
    （使用Spring的MockMvc by @AutoConfigureMockMvc注解）

--- hello-spring-boot-web/src/test/java/com/heller/hello/controller/HelloControllerTest.java
+++ hello-spring-boot-web/src/test/java/com/heller/hello/controller/HelloControllerTest.java
@@ -14,6 +14,19 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+/**
+ * 直接测试web层，不启动一个完整的web服务（其实也差不多了 almost the full stack）
+ * 使用Spring的MockMvc by @AutoConfigureMockMvc注解
+ *
+ * Another useful approach is to not start the server at all,
+ * but test only the layer below that,
+ * where Spring handles the incoming HTTP request and hands it off to your controller.
+ * That way, almost the full stack is used, and your code will be called exactly the same way
+ * as if it was processing a real HTTP request, but without the cost of starting the server.
+ *
+ * To do that we will use Spring’s MockMvc, and we can ask for that to be injected for us
+ * by using the @AutoConfigureMockMvc annotation on the test case:
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
