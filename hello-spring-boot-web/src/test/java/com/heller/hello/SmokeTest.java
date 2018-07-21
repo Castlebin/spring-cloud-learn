680fb5659bdf42f8991315faa5ff2eba3e1e8236
heller <heller@gmail.com>
2018-07-21 14:23:14 +0800

    添加一个简单的冒烟测试，测试Spring容器正确启动了

---
+++ hello-spring-boot-web/src/test/java/com/heller/hello/SmokeTest.java
@@ -0,0 +1,24 @@
+package com.heller.hello;
+
+import com.heller.hello.controller.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmokeTest {
+
+    @Autowired
+    private HelloController helloController;
+
+    @Test
+    public void contextLoads() {
+        assertThat(helloController).isNotNull();
+    }
+
+}
