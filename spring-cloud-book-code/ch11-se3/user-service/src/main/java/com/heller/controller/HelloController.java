52e906f9a58a6b0f3527e69f155ff786b10b4a33
Castlebin <Castlebin@gmail.com>
2018-07-22 18:36:48 +0800

    TODO 11. 服务链路追踪  (在zipkin界面上并没有看到链路数据啊，看来没成功，遗憾，后面再探索一把  )

--- spring-cloud-book-code/ch11-se3/user-service/src/main/java/com/heller/controller/HelloController.java
+++ spring-cloud-book-code/ch11-se3/user-service/src/main/java/com/heller/controller/HelloController.java
@@ -1,15 +1,18 @@
 package com.heller.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RequestMapping("/hello")
 @RestController
 public class HelloController {
 
     @GetMapping("/sayHello")
     public String sayHello(String name) {
+        log.info("request from " + name);
         return "Hello, " + name;
     }
 
