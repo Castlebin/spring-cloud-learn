552dd170e3465c51a98554faf999208e4ebceb79
Castlebin <castlebin@gmail.com>
2018-12-11 12:09:22 +0800

    使用TestRestTemplate来进行单元测试（启动了完整的web服务，并且完整的模拟了web请求）

--- spring-testing/gs-testing-web/src/main/java/com/heller/Application.java
+++ spring-testing/gs-testing-web/src/main/java/com/heller/Application.java
@@ -1,13 +1,16 @@
 package com.heller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        log.info("应用启动完毕...");
     }
 
 }
