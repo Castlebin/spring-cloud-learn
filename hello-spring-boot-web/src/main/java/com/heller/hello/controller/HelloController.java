49eb2b53316d8ea304e52229bc2d452027ec23a0
heller <heller@gmail.com>
2021-08-16 23:18:54 +0800

    SpringUtil

--- hello-spring-boot-web/src/main/java/com/heller/hello/controller/HelloController.java
+++ hello-spring-boot-web/src/main/java/com/heller/hello/controller/HelloController.java
@@ -1,19 +1,25 @@
 package com.heller.hello.controller;
 
-import com.heller.hello.model.entiry.User;
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
+import com.heller.hello.model.entiry.User;
+import com.heller.hello.util.SpringUtil;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RequestMapping("/hello")
 @RestController
 public class HelloController {
 
     @GetMapping("/sayHello")
     public String sayHello(String name) {
+        log.info("获取Spring上下文：{}", SpringUtil.getApplicationContext());
         return "Hello, " + name;
     }
 
